d08648b403f36e7ba78e7413c632292378df111f
Marik28 <69481006+Marik28@users.noreply.github.com>
2021-08-14 12:46:14 +0600

    теперь можно отслеживать несколько групп

--- src/telegram_bot/services/api.py
+++ src/telegram_bot/services/api.py
@@ -13,20 +13,22 @@ class ApiService:
         self.vk_api_url = settings.vk_api_url
         self.vk_api_token = settings.vk_api_token
         self.api_version = settings.api_version
-        self.group_id = -152811046
+        self.groups_to_track = [-152811046]
 
     async def get_latest_posts(self) -> list[WallPost]:
         """:raises ApiException"""
-        query = {
-            "owner_id": self.group_id,
-            "v": self.api_version,
-            "count": 100,
-            "access_token": self.vk_api_token,
-        }
-        response = await self.session.get(f"{self.vk_api_url}wall.get/", params=query)
-        if response.status == http.HTTPStatus.OK:
-            response_json = await response.json()
-            posts = [WallPost.parse_obj(post) for post in response_json["response"]["items"]]
-        else:
-            raise ApiException(f"Произошла непредвиденная ошибка.")
+        posts = []
+        for group_id in self.groups_to_track:
+            query = {
+                "owner_id": group_id,
+                "v": self.api_version,
+                "count": 100,
+                "access_token": self.vk_api_token,
+            }
+            response = await self.session.get(f"{self.vk_api_url}wall.get/", params=query)
+            if response.status == http.HTTPStatus.OK:
+                response_json = await response.json()
+                posts.extend([WallPost.parse_obj(post) for post in response_json["response"]["items"]])
+            else:
+                raise ApiException(f"Произошла непредвиденная ошибка.")
         return posts
